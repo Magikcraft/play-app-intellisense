a7f0e59f304a6e176f4098eb8bf9624a5b8cbb65
The Legendary Josh Wulf™ - As Seen on the Internet® <sitapati@worldsankirtan.net>
2017-10-07 09:59:31 +1000

    Update magikcraft.io.d.ts

--- magikcraft.io.d.ts
+++ magikcraft.io.d.ts
@@ -465,6 +465,7 @@ interface magik {
      * const magik = magikcraft.io;
      *
      * function remember() {
+     *      const thing = magik.exmemento();
      *      magik.dixit(thing.toString());
      * }
      * ```
